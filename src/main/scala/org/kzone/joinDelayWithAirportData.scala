48aa3696709e1519a7b8fd179474ed67949a1fbc
lraulier <lraulier@synotis.com>
2016-01-24 14:01:36 +0100

    ajout lib + code necessaire pour run spark

--- src/main/scala/org/kzone/joinDelayWithAirportData.scala
+++ src/main/scala/org/kzone/joinDelayWithAirportData.scala
@@ -1,8 +1,18 @@
 package org.kzone
 
+import org.apache.spark.{SparkContext, SparkConf}
+
 /**
   * Created by kubu-kzone on 1/24/16.
   */
 object joinDelayWithAirportData {
 
+  def main (args: Array[String]): Unit ={
+    val conf = new SparkConf().setAppName("afterlunch-draft")
+      .setMaster("local[2]")
+
+    val sparkContext = new SparkContext(conf)
+
+    println(sparkContext.version)
+  }
 }
