c8739d15b3fe6229e40fb83bfa9a92a822a8ce88
lraulier <lraulier@synotis.com>
2016-01-28 11:24:29 +0100

    ajout jionture sur cle mais fonctionne pas

--- src/main/scala/org/kzone/joinDelayWithAirportData.scala
+++ src/main/scala/org/kzone/joinDelayWithAirportData.scala
@@ -22,76 +22,17 @@ object joinDelayWithAirportData {
     val airportSplit = airport.map(line => line.split(","))
     //airportSplit.foreach(println)
     //sampleSplit.foreach(println)
-    val sampleKeyed = sampleSplit.keyBy(cells => cells(17))
+    val sampleKeyed = sampleSplit.keyBy(cells => "\""+cells(17)+"\"")
     val airportKeyed = airportSplit.keyBy(cells => cells(4))
+    //airportKeyed.foreach(println)
+    //sampleKeyed.foreach(println)
+    val joinedValue = sampleKeyed.join(airportKeyed).map(f => f._2._1(4))
+    joinedValue.take(3).foreach(println)
+    //joinedVal => joinedVal._2._1(1).toInt + joinedVal._2._2(1).toInt <= 1000)
+
   }
   def isHeader(line: String): Boolean = {
     line.contains("Year")
   }
-  def parseDelay(line: String) = {
-    val pieces = line.split(',')
-    val Year = pieces(0).toInt
-    val Month = pieces(1).toInt
-    val DayofMonth = pieces(2).toInt
-    val DayOfWeek = pieces(3).toInt
-    val DepTime = pieces(4).toInt
-    val CRSDepTime = pieces(5).toInt
-    val ArrTime = pieces(6).toInt
-    val CRSArrTime = pieces(7).toInt
-    val UniqueCarrier = pieces(8)
-    val FlightNum = pieces(9).toInt
-    val TailNum = pieces(10)
-    val ActualElapsedTime = pieces(11).toInt
-    val CRSElapsedTime = pieces(12).toInt
-    val AirTime = pieces(13).toInt
-    val ArrDelay = pieces(14).toInt
-    val DepDelay = pieces(15).toInt
-    val Origin = pieces(16)
-    val Dest = pieces(17)
-    val Distance = pieces(18).toInt
-    val TaxiIn = pieces(19).toInt
-    val TaxiOut = pieces(20).toInt
-    val Cancelled = pieces(21).toInt
-    val CancellationCode = pieces(22).toInt
-    val Diverted = pieces(23).toInt
-    val CarrierDelay = pieces(24).toInt
-    val WeatherDelay = pieces(25).toInt
-    val NASDelay = pieces(26).toInt
-    val SecurityDelay = pieces(27).toInt
-    val LateAircraftDelay = pieces(28).toInt
 
-    Delay(Year,Month,DayofMonth,DayOfWeek,DepTime,CRSDepTime,ArrTime,CRSArrTime,UniqueCarrier,FlightNum,TailNum,ActualElapsedTime,
-      CRSElapsedTime,AirTime,ArrDelay,DepDelay,Origin,Dest,Distance,TaxiIn,TaxiOut,Cancelled,CancellationCode,Diverted,CarrierDelay,WeatherDelay,
-      NASDelay,SecurityDelay,LateAircraftDelay)
-  }
- case class Delay(Year: Int,
-            Month: Int,
-            DayofMonth: Int,
-            DayOfWeek: Int,
-            DepTime: Int,
-            CRSDepTime: Int,
-            ArrTime: Int,
-            CRSArrTime: Int,
-            UniqueCarrier: String,
-            FlightNum: Int,
-            TailNum: String,
-            ActualElapsedTime: Int,
-            CRSElapsedTime: Int,
-            AirTime: Int,
-            ArrDelay: Int,
-            DepDelay: Int,
-            Origin: String,
-            Dest: String,
-            Distance: Int,
-            TaxiIn: Int,
-            TaxiOut: Int,
-            Cancelled: Int,
-            CancellationCode: Int,
-            Diverted: Int,
-            CarrierDelay: Int,
-            WeatherDelay: Int,
-            NASDelay: Int,
-            SecurityDelay: Int,
-            LateAircraftDelay: Int)
-  
 }
