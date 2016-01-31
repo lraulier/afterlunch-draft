a3b8d5a8acee55dd5abcadf5dda828eaa1297a43
lraulier <lraulier@synotis.com>
2016-01-31 20:24:26 +0100

    suppression case class pas necessaire + registerTempTable => select

--- src/main/scala/org/kzone/joinDelayWithAirportData.scala
+++ src/main/scala/org/kzone/joinDelayWithAirportData.scala
@@ -22,36 +22,8 @@ object joinDelayWithAirportData {
       .option("inferSchema", "true") // Automatically infer data types
       .load("../data/sample.csv")
     df.show() // show content
-
-    // define case class Delay
-    case class Delay(Year: String,
-                     Month: String,
-                     DayofMonth: String,
-                     DayOfWeek: String,
-                     DepTime: String,
-                     CRSDepTime: String,
-                     ArrTime: String,
-                     CRSArrTime: String,
-                     UniqueCarrier: String,
-                     FlightNum: String,
-                     TailNum: String,
-                     ActualElapsedTime: String,
-                     CRSElapsedTime: String,
-                     AirTime: String,
-                     ArrDelay: String,
-                     DepDelay: String,
-                     Origin: String,
-                     Dest: String,
-                     Distance: String,
-                     TaxiIn: String,
-                     TaxiOut: String,
-                     Cancelled: String,
-                     CancellationCode: String,
-                     Diverted: String,
-                     CarrierDelay: String,
-                     WeatherDelay: String,
-                     NASDelay: String,
-                     SecurityDelay: String,
-                     LateAircraftDelay: String)
+    df.printSchema()
+    df.registerTempTable("delay")
+    sqlContext.sql("select Year from delay").foreach(println)
   }
 }
