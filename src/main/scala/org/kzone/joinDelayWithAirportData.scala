3ad03233d4490bdbf611b0be8573007d5eed984f
lraulier <lraulier@synotis.com>
2016-01-31 22:34:51 +0100

    select distinct(C2) from delay,airport where airport.C4=delay.Origin

--- src/main/scala/org/kzone/joinDelayWithAirportData.scala
+++ src/main/scala/org/kzone/joinDelayWithAirportData.scala
@@ -21,7 +21,7 @@ object joinDelayWithAirportData {
       .option("header", "true") // Use first line of all files as header
       .option("inferSchema", "true") // Automatically infer data types
       .load("../data/sample.csv")
-    //delay.show() // show content
+    delay.show() // show content
     //delay.printSchema()
     delay.registerTempTable("delay")
     //sqlContext.sql("select Year from delay").foreach(println)
@@ -32,7 +32,9 @@ object joinDelayWithAirportData {
       .option("inferSchema", "true")
       .load("../data/airport_data.csv")
     //airport.printSchema()
-    //airport.show()
+    airport.show()
     airport.registerTempTable("airport")
+
+    sqlContext.sql("select distinct(C2) from delay,airport where airport.C4=delay.Origin ").foreach(println)
   }
 }
